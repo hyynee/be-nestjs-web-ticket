af338c310cd580f9e266ab154f843e52e559f3e2
hyynee <122151727+hyynee@users.noreply.github.com>
2026-03-03 22:26:06 +0700

    fix: JwtStrategy

--- src/strategy/jwt.strategy.ts
+++ src/strategy/jwt.strategy.ts
@@ -1,38 +1,17 @@
-// import { Injectable } from '@nestjs/common';
-// import { ConfigService } from '@nestjs/config';
-// import { PassportStrategy } from '@nestjs/passport';
-// import { jwtConstants } from '@src/auth/constants';
-// import { ExtractJwt, Strategy } from 'passport-jwt';
 
-// @Injectable()
-// export class JwtStrategy extends PassportStrategy(Strategy) {
-//   // Bearer token
-//   constructor(config: ConfigService) {
-//     super({
-//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-//       ignoreExpiration: false,
-//       secretOrKey: jwtConstants.secret,
-//     });
-//   }
-//   async validate(payload: any) {
-//     return payload;
-//   }
-// }
 
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
-import { jwtConstants } from "@src/auth/constants";
 import { ExtractJwt, Strategy } from "passport-jwt";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(config: ConfigService) {
+  constructor(private config: ConfigService) {
     super({
-      // Đọc JWT từ Bearer header (cho cả login email/password và Google OAuth)
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: jwtConstants.secret as string,
+      secretOrKey: config.getOrThrow<string>("SECRET_KEY"),
     });
   }
 
